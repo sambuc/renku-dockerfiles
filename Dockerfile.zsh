9055f997d03bf217991dd7b9eafaa86064e0afe0
Lionel Sambuc <lionel.sambuc@epfl.ch>
2025-07-04 11:39:43 +0200

    fix: Add git-lfs

--- Dockerfile.zsh
+++ Dockerfile.zsh
@@ -5,7 +5,7 @@ SHELL ["/bin/sh", "-c"]
 USER root
 
 # LSA: Bare minimum tools to get to a working environment
-RUN apk add --no-cache zsh tree bat dust openssh git vim htop
+RUN apk add --no-cache zsh tree bat dust openssh git git-lfs vim htop
 
 # LSA: Set zsh as login shell
 RUN sed -e '/^renku/s,bash,zsh,' -i /etc/passwd
